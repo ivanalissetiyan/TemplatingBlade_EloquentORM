d177ce13ad23a48ba6b2072473a5c8c459d6bef8
ivanalissetiyan <ivanalissetiyan1998@gmail.com>
2022-02-06 18:15:44 +0700

    add foreign keys

---
+++ database/migrations/2022_02_06_105235_add_foreign_keys_to_clubs.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddForeignKeysToClubs extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('clubs', function (Blueprint $table) {
+            $table->foreign('stadia_id', 'stadia_id_fk1')->references('id')
+                ->on('stadia')->onUpdate('CASCADE')->onDelete('RESTRICT');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('clubs', function (Blueprint $table) {
+            $table->dropForeign('stadia_id_fk1');
+        });
+    }
+}
